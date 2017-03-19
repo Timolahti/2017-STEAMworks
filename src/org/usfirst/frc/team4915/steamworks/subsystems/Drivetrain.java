a800e1cd57a2313dac5f2956c39843132ab8925b
Timolahti <timolahtinen@msn.com>
2017-03-19 14:49:16 -0700

    added square inputs

--- src/org/usfirst/frc/team4915/steamworks/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team4915/steamworks/subsystems/Drivetrain.java
@@ -551,7 +551,7 @@ public class Drivetrain extends SpartronicsSubsystem
                     m_replayForward.add(forward); 
                     m_replayRotation.add(rotation);
                 }
-                m_robotDrive.arcadeDrive(forward, rotation);
+                m_robotDrive.arcadeDrive(forward, rotation, true /*square inputs */);
             }
             else
             {
